bc8a795ddc055b6a9bddabc9c8bd945467710138
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-07-14 11:54:46 +0800

    feat(backend): restructure application entry point

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,33 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
+
+use crate::{cmd::SchedulerCmd, scheduler::core::init_scheduler};
+
+mod cmd;
+mod config;
+mod core;
+mod scheduler;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tokio::spawn(async move {
+                let app_config = config::load_config(&handle).await;
+                let shared_config = config::SharedConfig::new(app_config.clone());
+                handle.manage(shared_config);
+
+                let cmd_tx = init_scheduler(handle.clone()).await;
+                handle.manage(SchedulerCmd(cmd_tx));
+            });
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            cmd::pause_scheduler,
+            cmd::resume_scheduler,
+            cmd::postpone_break,
+            cmd::save_config
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
