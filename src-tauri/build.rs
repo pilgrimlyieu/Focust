9d2647c2843b3f455e667836126d69cdc1911431
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-11-11 10:29:32 +0800

    build: fix CI complaining

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,14 +1,18 @@
 // https://github.com/tauri-apps/tauri/issues/13419#issuecomment-3398457618
 // Fix `STATUS_ENTRYPOINT_NOT_FOUND` error on Windows when testing.
 fn main() {
-    let mut attributes = tauri_build::Attributes::new();
     #[cfg(windows)]
     {
+        let mut attributes = tauri_build::Attributes::new();
         attributes = attributes
             .windows_attributes(tauri_build::WindowsAttributes::new_without_app_manifest());
         add_manifest();
+        tauri_build::try_build(attributes).unwrap();
     }
-    tauri_build::try_build(attributes).unwrap();
+    #[cfg(not(windows))]
+    {
+        tauri_build::build();
+    }
 }
 
 #[cfg(windows)]
